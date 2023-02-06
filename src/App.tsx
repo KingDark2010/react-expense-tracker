85ca8dea7221195b59e5d303d618b614800fc3be
KingDark2010 <ahmed-ahmed83356@hotmail.com>
2023-02-06 12:53:53 +0200

    create project base files

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Header from './App/Components/header.component'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to React app
+        </p>
+        <Header></Header>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/Components/header.component.tsx
@@ -0,0 +1,7 @@
+const Header = () =>{
+    return (
+        <h1>Hello World from header !</h1>
+    );
+};
+
+export default Header;
